379a93c5b0c4cdac332620fa4df029e30f5935b9
Claudio-Arauj <claudiopereira2004@gmail.com>
2025-01-18 13:52:23 -0300

    Criando Receita 17 - NextJS

---
+++ receita17-nextjs/app/novarota/subdiretorio/componentesub.js
@@ -0,0 +1,10 @@
+export function Condicao(){
+
+    return (
+
+        <div>
+            <br/>
+            <p>Obs: Ela não morreu de fato, continua bem, apenas seu marido se encontra em estado vegetativo</p>
+        </div>
+    ) 
+}
